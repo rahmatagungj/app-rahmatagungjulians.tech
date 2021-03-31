6d2c4ab395d80cf32f41e9c17aa22b8521bf48f9
rahmatagungj <rahmatagungj@gmail.com>
2021-04-01 03:19:57 +0700

    :art: make deployment ready

---
+++ assets/js/footer.dev.js
@@ -0,0 +1,15 @@
+// PWA
+if ("serviceWorker" in navigator) {
+  if (navigator.serviceWorker.controller) {
+    // console.log("An active service worker found, no need to register");
+  } else {
+    // Register the service worker
+    navigator.serviceWorker
+      .register("./sw.js", {
+        scope: "./",
+      })
+      .then(function (reg) {
+        // console.log("Service worker has been registered for scope: " + reg.scope);
+      });
+  }
+}
